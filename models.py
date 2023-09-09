0982d5ee5ccbd0c8292b874beecb0555afb63681
sidee <said.issack@student.moringaschool.com>
2023-09-09 18:40:00 +0300

    completed

--- models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Table
-from sqlalchemy.orm import relationship, backref
-
-from sqlalchemy.ext.declarative import declarative_base
-
-from database import session
-
-Base = declarative_base()
-
